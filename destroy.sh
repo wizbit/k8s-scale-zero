41bf3ef5ad84b78402b904b32cf402c9b9d7c77f
Marcus Speight <marcus.speight@hotmail.co.uk>
2026-05-20 18:51:04 +0100

    Add destroy script Remove file containing duplicates

--- destroy.sh
+++ destroy.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
 
+CONTEXT=${KUBECTL_CONTEXT:-"k3d-cluster1"}
+
+kubectl delete -f ./demo-app.yaml --namespace demo --context "$CONTEXT"
+kubectl delete -f ./interceptor-route.yaml --namespace demo --context "$CONTEXT"
+kubectl delete -f ./scaled-object.yaml --namespace demo --context "$CONTEXT"
+kubectl delete -f ./ingress.yaml --namespace demo --context "$CONTEXT"
+kubectl delete namespace demo --context "$CONTEXT"
